7129bac01c6d5245e4542b757c2161c4271a85e3
Eric Zhang <ericbkzhang@gmail.com>
2026-02-28 10:31:45 -0500

    Finish background and text for Connect With Us

--- src/components/connect-with-us/connect-with-us.tsx
+++ src/components/connect-with-us/connect-with-us.tsx
@@ -2,9 +2,26 @@ import type { ReactNode } from "react";
 
 const ConnectWithUs = (): ReactNode => {
     return (
-        <section id="connect-with-us">
-            <div className="w-full h-32 bg-neutral-800">
-                <h1>Hello world</h1>
+        <section
+            id="connect-with-us"
+            className="w-full h-dvh
+                       flex flex-col justify-center items-center
+                       rounded-2xl
+                       bg-neutral-800 text-center text-white"
+        >
+            <div
+                className="space-y-6 
+                            text-center font-family-[Manrope] text-white"
+            >
+                <button
+                    id="connect-with-us-button"
+                    className="border-1 border-white rounded-sm py-2 px-4"
+                >
+                    Members
+                </button>
+
+                <h2 className="text-5xl font-semibold">Connect With Us</h2>
+                <h4>The students behind our Mission</h4>
             </div>
         </section>
     );
