07169d545898bd7de7190f5d4e4b125f23fec36f
Eric Zhang <ericbkzhang@gmail.com>
2026-02-28 09:12:34 -0500

    Install Tailwind

--- src/components/connect-with-us/connect-with-us.tsx
+++ src/components/connect-with-us/connect-with-us.tsx
@@ -1,7 +1,13 @@
 import type { ReactNode } from "react";
 
 const ConnectWithUs = (): ReactNode => {
-    return <></>;
+    return (
+        <section id="connect-with-us">
+            <div className="w-full h-32 bg-neutral-800">
+                <h1>Hello world</h1>
+            </div>
+        </section>
+    );
 };
 
 export default ConnectWithUs;
